0ec5333a51506e36ee2822372a5021854a4782e0
Andrzej <ahankus91@gmail.com>
2024-10-14 09:15:35 +0200

    rabbit training

---
+++ Consumer/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Text.Json;
+
+class Program
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            UserName = "user",
+            Password = "password",
+        };
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare(queue: "hello",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            // Deserialize JSON back to Message object
+            var messageObject = JsonSerializer.Deserialize<Message>(message);
+
+            Console.WriteLine(" [x] Received: {0}, Timestamp: {1}", messageObject.Text, messageObject.Timestamp);
+        };
+        channel.BasicConsume(queue: "hello",
+                             autoAck: true,
+                             consumer: consumer);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Text.Json;
+
+class Program
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            UserName = "user",
+            Password = "password",
+        };
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare(queue: "hello",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+        var messageObject = new Message
+        {
+            Text = "Hello World!",
+            Timestamp = DateTime.Now
+        };
+
+        string message = JsonSerializer.Serialize(messageObject);
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "",
+                             routingKey: "hello",
+                             basicProperties: null,
+                             body: body);
+
+        Console.WriteLine(" [x] Sent {0}", message);
+    }
+}
